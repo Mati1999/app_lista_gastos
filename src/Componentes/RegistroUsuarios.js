343e8ecd217b35afc0de6240b618ba6c9cd9a517
Matias Aguilera <aguileramati50@gmail.com>
2021-01-10 16:11:02 -0300

    agregamos logica para corroborar correo y contraseña

--- src/Componentes/RegistroUsuarios.js
+++ src/Componentes/RegistroUsuarios.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Header, Titulo, ContenedorHeader } from './../Elementos/Header';
 import Boton from './../Elementos/Boton';
@@ -14,6 +14,49 @@ margin-bottom:1.25rem;
 `;
 
 const RegistroUsuarios = () => {
+
+    const [correo, establecerCorreo] = useState('')
+    const [password, establecerPassword] = useState('')
+    const [password2, establecerPassword2] = useState('')
+
+    const handleChange = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                establecerCorreo(e.target.value);
+                break;
+            case 'password':
+                establecerPassword(e.target.value);
+                break;
+            case 'password2':
+                establecerPassword2(e.target.value);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Comprobamos del lado del cliente que el correo sea  valido.
+
+        const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+        if (!expresionRegular.test(correo)) {
+            console.log('Ingresa un correo valido');
+            return;
+        };
+        if (correo === "" || password === "" || password2 === "") {
+            console.log("Porr favor rellene todos los datos");
+            return;
+        }
+        if (password !== password2) {
+            console.log('Las contraseñas no son iguales');
+            return;
+        }
+        console.log('Registramos usuario');
+    }
+
     return (
         <>
             <Helmet>
@@ -29,11 +72,11 @@ const RegistroUsuarios = () => {
                 </ContenedorHeader>
             </Header>
 
-            <Formulario>
+            <Formulario onSubmit={handleSubmit}>
                 <Svg />
-                <Input type="email" name="email" placeholder="Correo Electronico" />
-                <Input type="password" name="password" placeholder="Contraseña" />
-                <Input type="password" name="password2" placeholder="Repetir Contraseña" />
+                <Input type="email" name="email" placeholder="Correo Electronico" value={correo} onChange={handleChange} />
+                <Input type="password" name="password" placeholder="Contraseña" value={password} onChange={handleChange} />
+                <Input type="password" name="password2" placeholder="Repetir Contraseña" value={password2} onChange={handleChange} />
                 <ContenedorBoton>
                     <Boton as="button" primario type="submit">Crear Cuenta</Boton>
                 </ContenedorBoton>
